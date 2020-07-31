8ce20c8f102a48bc7c3c8f8e802bee83a16163ed
Vrushant <vrushantdomde@gmail.com>
2020-08-01 00:40:13 +0530

    Updates

---
+++ precache-manifest.cd61339b0710c53c02ae5e03bf78e52f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4a8213214bdae459f8d40b7a0bc99f",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "6509ff5907c1eed26948",
+    "url": "/ReactProject/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d76f6e1c3896c5c433db",
+    "url": "/ReactProject/static/css/main.f58c0295.chunk.css"
+  },
+  {
+    "revision": "6509ff5907c1eed26948",
+    "url": "/ReactProject/static/js/2.61c552e6.chunk.js"
+  },
+  {
+    "revision": "6e91efd442ad4883f2889abead8c8f89",
+    "url": "/ReactProject/static/js/2.61c552e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76f6e1c3896c5c433db",
+    "url": "/ReactProject/static/js/main.71e3ae59.chunk.js"
+  },
+  {
+    "revision": "7524b5bf385f1bbac8ed",
+    "url": "/ReactProject/static/js/runtime-main.3f7b3336.js"
+  },
+  {
+    "revision": "59dd9160347dc4dd48c115d05b850b4d",
+    "url": "/ReactProject/static/media/Notelogo.svg.59dd9160.png"
+  },
+  {
+    "revision": "efafe528809d2168e10b72ff0266b1aa",
+    "url": "/ReactProject/static/media/ag.efafe528.jpg"
+  },
+  {
+    "revision": "28929f5507b05939857542d05acc0dba",
+    "url": "/ReactProject/static/media/back4.28929f55.jpg"
+  },
+  {
+    "revision": "1220d76149ef9a58b4ca0b0029dd963e",
+    "url": "/ReactProject/static/media/wall1.1220d761.jpg"
+  },
+  {
+    "revision": "8a64736eed015fc3cc25e67e26027cab",
+    "url": "/ReactProject/static/media/wall2.8a64736e.jpg"
+  },
+  {
+    "revision": "9b4d2607f89447aec2eaebcffe3a16f9",
+    "url": "/ReactProject/static/media/wall3.9b4d2607.jpg"
+  },
+  {
+    "revision": "9732ae3baf7a904d037c02a598ac86e7",
+    "url": "/ReactProject/static/media/wall4.9732ae3b.jpg"
+  },
+  {
+    "revision": "aa4769e5c4314fa7b37c57e3567267ee",
+    "url": "/ReactProject/static/media/wall5.aa4769e5.jpg"
+  }
+]);
